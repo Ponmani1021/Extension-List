73d4282a29e3709f7747001e027fb9db7fc37fd9
Ponmani1021 <ponmani1021@gmail.com>
2025-06-11 17:25:38 +0530

    added changes

---
+++ EList.js
@@ -0,0 +1,17 @@
+// Theme
+
+// function themeChange(){
+//     const body = document.body
+//     const image = document.querySelector(".themeimg")
+
+//     if(body.classList.contains("dark")){
+//        body.classList.remove("dark")
+//        body.classList.add("light")
+//        image.src = "../ExtensionList/images/icon-moon.svg"
+//     }
+//     else{
+//         body.classList.remove("light")
+//         body.classList.add("dark")
+//         image.src = "../ExtensionList/images/icon-sun.svg"
+//     }
+// }
